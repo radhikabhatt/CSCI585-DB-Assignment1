aaac07d5e9183f3d0d40ba0100939ab35d6fa875
rads <rads.bhatt@gmail.com>
2017-05-11 23:02:19 -0700

    SQL querries

---
+++ Q1.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE HotelStays(
+ roomNum INTEGER NOT NULL,
+ during tsrange NOT NULL,
+ guestName CHAR(30) NOT NULL,
+ EXCLUDE USING gist(roomNum WITH =, during WITH&&)
+);
+
+INSERT INTO HotelStays VALUES(123, '[2010-01-01, 2010-02-01]', 'Test User1')
+INSERT INTO HotelStays VALUES(124, '[2010-01-01, 2010-02-01]', 'Test User1')
